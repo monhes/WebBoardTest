10f09c35e0ce968b05822ca0916343ab8d7ddb07
thud aneknual <monhomes@hotmail.com>
2022-12-16 04:32:36 +0700

    delete unused component

--- web-board-test/src/app/app.module.ts
+++ web-board-test/src/app/app.module.ts
@@ -11,7 +11,6 @@ import { UserLoginComponent } from './Login/user-login/user-login.component';
 import { TopicContentComponent } from './Topic-content/topic-content/topic-content.component';
 import { TopicCreateComponent } from './Topic-create/topic-create/topic-create.component';
 import { TopicListComponent } from './Topic-list/topic-list/topic-list.component';
-import { UserRegistComponent } from './Regist/user-regist/user-regist.component';
 
 
 @NgModule({
@@ -21,7 +20,6 @@ import { UserRegistComponent } from './Regist/user-regist/user-regist.component'
     TopicContentComponent,
     TopicCreateComponent,
     TopicListComponent,
-    UserRegistComponent
   ],
   imports: [
     HttpClientModule,
